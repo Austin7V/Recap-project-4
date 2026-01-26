c82bb2a15daf0e17df8b8bb389841905af611ba4
Sergey Badin <sergeykfg1@me.com>
2026-01-26 15:45:52 +0100

    feat: add ColorForm component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color.jsx";
 import "./App.css";
 
+import ColorForm from "./Components/ColorForm/ColorForm.jsx";
+
 function App() {
   //Farben abspeichern damit später neue Farben hinzufügen.
   const [colors, setColors] = useState(initialColors);
@@ -10,6 +12,8 @@ function App() {
   return (
     <>
       <h1>Theme Creator</h1>
+
+      <ColorForm onSubmitColor={(data) => console.log(data)} />
 
       {colors.map((color) => (
         <Color key={color.id} color={color} />
