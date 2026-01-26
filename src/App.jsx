355651acaf666e1233a2083972bc0280dcb79ff2
Sergey Badin <sergeykfg1@me.com>
2026-01-26 11:00:32 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { initialColors } from "./lib/colors";
-import Color from "./Components/Color/Color";
+import Color from "./Components/Color/Color.jsx";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Theme Creator</h1>
-      // display colors here
+      display colors here
     </>
   );
 }
