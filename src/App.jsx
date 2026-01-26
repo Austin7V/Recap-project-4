9caeaf0b0e68f96f78ab5ddc2ea65b05c4c96044
Sergey Badin <sergeykfg1@me.com>
2026-01-26 15:32:35 +0100

    refactor: move colors list into state + add ColorForm and ColorInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color.jsx";
 import "./App.css";
 
 function App() {
+  //Farben abspeichern damit später neue Farben hinzufügen.
+  const [colors, setColors] = useState(initialColors);
+
   return (
     <>
       <h1>Theme Creator</h1>
-      {initialColors.map((color) => (
+
+      {colors.map((color) => (
         <Color key={color.id} color={color} />
       ))}
     </>
