dcfba41d55a9790be7fd7e434d60a8563c0a291c
Sergey Badin <sergeykfg1@me.com>
2026-01-26 16:44:38 +0100

    feat: complete add color flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { nanoid } from "nanoid";
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color.jsx";
 import "./App.css";
@@ -8,12 +9,21 @@ import ColorForm from "./Components/ColorForm/ColorForm.jsx";
 function App() {
   //Farben abspeichern damit später neue Farben hinzufügen.
   const [colors, setColors] = useState(initialColors);
+  function handleAddColor(data) {
+    const newColor = {
+      id: nanoid(),
+      role: data.role,
+      hex: data.hex,
+      contrastText: data.contrastText,
+    };
+    setColors((prevColors) => [newColor, ...prevColors]);
+  }
 
   return (
     <>
       <h1>Theme Creator</h1>
 
-      <ColorForm onSubmitColor={(data) => console.log(data)} />
+      <ColorForm onSubmitColor={handleAddColor} />
 
       {colors.map((color) => (
         <Color key={color.id} color={color} />
