39482850320865f5735185d27ba421ef5c34ba9a
Sergey Badin <sergeykfg1@me.com>
2026-01-28 19:24:18 +0100

    feat: copy hex code to clipboard

--- src/Components/CopyToClipboard/CopyToClipboard.jsx
+++ src/Components/CopyToClipboard/CopyToClipboard.jsx
@@ -1,3 +1,18 @@
+import { useState } from "react";
+
 export default function CopyToClipboard({ textToCopy }) {
-  return <button type="button">Copy</button>;
+  const [copied, setCopied] = useState(false);
+  async function handleCopie() {
+    await navigator.clipboard.writeText(textToCopy);
+
+    setCopied(true);
+  }
+  return (
+    <>
+      <button type="button" onClick={handleCopie}>
+        Copy
+      </button>
+      ;{copied && <p>Copied!</p>}
+    </>
+  );
 }
